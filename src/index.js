49cadc0150bfa000c9f26e36a5a85a45a179cb15
dagm-ey <dagmey33@gmail.com>
2024-02-14 13:46:45 -0800

    installed react-router-dom and added more components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
+import './index.scss';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
+
+
+reportWebVitals();
---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,25 @@
+import { Fragment } from 'react';
+import { Outlet, Link} from 'react-router-dom';
+import './navigation.styles.scss';
+
+import { ReactComponent as CrwnLogo } from '../../assets/007 crown.svg';
+
+const Navigation = () => {
+    return(
+    <Fragment>
+        <div className='navigation'>
+            <Link className='logo-container' to='/'>
+                <CrwnLogo className='logo'/>
+            </Link>
+            <div className='nav-links-container'>
+                <Link className='nav-link' to='/shop'>
+                    SHOP
+                </Link>
+            </div>
+        </div>
+      <Outlet />
+    </Fragment>
+    )
+  };
+
+  export default Navigation;
